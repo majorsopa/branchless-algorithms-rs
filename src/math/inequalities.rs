872fd3d5ba303a962772ff0d95a985ca1bb1f571
majorsopa <73245063+majorsopa@users.noreply.github.com>
2022-01-15 02:58:27 +0000

    inequalities returning 1 or 0

--- src/math/inequalities.rs
+++ src/math/inequalities.rs
@@ -8,6 +8,16 @@ pub fn less_than(a: isize, b: isize) -> usize {
   (super::minimum::minimum(a, b) ^ b) as usize
 }
 
+// returns one if a is greater than b and zero otherwise
+pub fn greater_than_as_one(a: isize, b: isize) -> usize {
+  super::zero_checks::check_non_zero_unsigned(greater_than(a, b))
+}
+
+// returns one if a is less than b and zero otherwise
+pub fn less_than_as_one(a: isize, b: isize) -> usize {
+  super::zero_checks::check_non_zero_unsigned(less_than(a, b))
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -25,6 +35,18 @@ mod tests {
   }
 
   #[test]
+  fn check_greater_than_as_one() {
+    assert_eq!(0, greater_than_as_one(1, 2));
+    assert_eq!(0, greater_than_as_one(-3, 4));
+    assert_eq!(0, greater_than_as_one(-6, -1));
+    assert_eq!(0, greater_than_as_one(5, 5));
+
+    assert_eq!(1, greater_than_as_one(6, 2));
+    assert_eq!(1, greater_than_as_one(100, -5));
+    assert_eq!(1, greater_than_as_one(-32, -600));
+  }
+
+  #[test]
   fn check_less_than() {
     assert_eq!(0, less_than(2, 1));
     assert_eq!(0, less_than(4, -3));
@@ -34,5 +56,17 @@ mod tests {
     assert_ne!(0, less_than(2, 6));
     assert_ne!(0, less_than(-5, 100));
     assert_ne!(0, less_than(-600, -32));
+  }
+
+  #[test]
+  fn check_less_than_as_one() {
+    assert_eq!(0, less_than_as_one(2, 1));
+    assert_eq!(0, less_than_as_one(4, -3));
+    assert_eq!(0, less_than_as_one(-1, -6));
+    assert_eq!(0, less_than_as_one(5, 5));
+
+    assert_eq!(1, less_than_as_one(2, 6));
+    assert_eq!(1, less_than_as_one(-5, 100));
+    assert_eq!(1, less_than_as_one(-600, -32));
   }
 }
